9b965e9223901e58a74ab803d21fd08368f6377a
TY <ty@sentio.xyz>
2023-04-13 13:02:11 +0800

    fix(rpc): eth_blockNumber returns unsynced block number

--- turbo/rpchelper/rpc_block.go
+++ turbo/rpchelper/rpc_block.go
@@ -17,20 +17,23 @@ var UnknownBlockError = &rpc.CustomError{
 }
 
 func GetLatestBlockNumber(tx kv.Tx) (uint64, error) {
+	headHash := rawdb.ReadHeadBlockHash(tx)
+	headNumber := rawdb.ReadHeaderNumber(tx, headHash)
+	if headNumber == nil {
+		return 0, fmt.Errorf("getting latest block number: head is not set")
+	}
+
+	// On PoS networks, the forkchoice head is the latest block that has been finalized.
+	// Only exception is when syncing (header updated but the rest have not yet), in which case we use the head block instead.
 	forkchoiceHeadHash := rawdb.ReadForkchoiceHead(tx)
 	if forkchoiceHeadHash != (libcommon.Hash{}) {
 		forkchoiceHeadNum := rawdb.ReadHeaderNumber(tx, forkchoiceHeadHash)
-		if forkchoiceHeadNum != nil {
+		if forkchoiceHeadNum != nil && *forkchoiceHeadNum <= *headNumber {
 			return *forkchoiceHeadNum, nil
 		}
 	}
 
-	blockNum, err := stages.GetStageProgress(tx, stages.Execution)
-	if err != nil {
-		return 0, fmt.Errorf("getting latest block number: %w", err)
-	}
-
-	return blockNum, nil
+	return *headNumber, nil
 }
 
 func GetFinalizedBlockNumber(tx kv.Tx) (uint64, error) {
