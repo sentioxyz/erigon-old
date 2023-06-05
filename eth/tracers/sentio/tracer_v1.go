c287e9f5b7630e8979d155eb530be6b5dfe4a7cb
Fuyao Zhao <zfy0701@gmail.com>
2023-06-05 09:03:26 +0800

    filter function entry jump

--- eth/tracers/sentio/tracer_v1.go
+++ eth/tracers/sentio/tracer_v1.go
@@ -10,71 +10,23 @@ import (
 	"github.com/ledgerwatch/erigon/common/math"
 	"github.com/ledgerwatch/erigon/core/vm"
 	"github.com/ledgerwatch/erigon/eth/tracers"
-	"github.com/ledgerwatch/log/v3"
 )
-
-type functionConfig struct {
-	Pc           uint64 `json:"pc"`
-	InputMemory  bool   `json:"inputMemory"`
-	OutputMemory bool   `json:"outputMemory"`
-}
-
-type sentioTracerConfig struct {
-	Functions map[string][]functionConfig `json:"functions"`
-}
 
 func init() {
 	tracers.RegisterLookup(false, newSentioTracer)
 }
 
 type sentioTracer struct {
-	config            sentioTracerConfig
 	env               vm.VMInterface
 	activePrecompiles []libcommon.Address
 
-	traces    []Trace
-	descended bool
-	index     int
-	//currentDepth int
+	traces      []Trace
+	descended   bool
+	index       int
 	currentGas  math.HexOrDecimal64
 	callsNumber int
 	rootTrace   Trace
 	gasLimit    uint64
-}
-
-type TraceBase interface {
-}
-
-type Trace struct {
-	//op      vm.OpCode
-	Type    string              `json:"type"`
-	Pc      uint64              `json:"pc"`
-	Index   int                 `json:"index"`
-	GasIn   math.HexOrDecimal64 `json:"gasIn"` // TODO this should be hex
-	Gas     math.HexOrDecimal64 `json:"gas"`
-	GasCost math.HexOrDecimal64 `json:"gasCost"`
-	GasUsed math.HexOrDecimal64 `json:"gasUsed"`
-	Output  hexutil.Bytes       `json:"output,omitempty"`
-	From    *libcommon.Address  `json:"from,omitempty"`
-
-	// Used by call
-	To          *libcommon.Address `json:"to,omitempty"`
-	Input       string             `json:"input,omitempty"` // TODO better struct it and make it bytes
-	Value       hexutil.Bytes      `json:"value"`
-	ErrorString string             `json:"error,omitempty"`
-
-	// Used by jump
-	Stack []uint256.Int `json:"stack,omitempty"`
-	//Stack  [][4]uint64 `json:"stack,omitempty"`
-	Memory []byte `json:"memory,omitempty"`
-
-	// Used by log
-	Address *libcommon.Address `json:"address,omitempty"`
-	Data    hexutil.Bytes      `json:"data,omitempty"`
-	Topics  []hexutil.Bytes    `json:"topics,omitempty"`
-
-	// Only used by root
-	Traces []Trace `json:"traces,omitempty"`
 }
 
 func (t *sentioTracer) CaptureTxStart(gasLimit uint64) {
@@ -202,18 +154,9 @@ func (t *sentioTracer) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64, s
 		return
 	case vm.JUMP, vm.JUMPDEST:
 		from := scope.Contract.Address()
-
-		//var stack []uint256.Int
-		//for i := 0; i < scope.Stack.Len(); i++ {
-		//	stack = append(stack, scope.Stack.Data[i])
-		//}
-
 		jump := mergeBase(Trace{
-			From: &from,
-			//Stack: stack,
-			Stack: append([]uint256.Int(nil), scope.Stack.Data...),
-
-			//Memory: scope.Memory.GetCopy(0, int64(scope.Memory.Len())),
+			From:  &from,
+			Stack: append([]uint256.Int(nil), scope.Stack.Data...), // TODO only need partial
 		})
 		t.traces = append(t.traces, jump)
 		return
@@ -272,40 +215,6 @@ func (t *sentioTracer) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64, s
 		}
 		t.traces = append(t.traces, trace)
 	}
-
-	//t.currentDepth = depth
-
-	//call := t.traces[topIdx]
-	//t.traces = t.traces[0:topIdx]
-	//topIdx--
-
-	//if call.op == vm.CREATE || call.op == vm.CREATE2 {
-	//	if call.GasIn > 0 {
-	//		call.GasUsed = call.GasIn - call.GasCost + gas // TODO double check this
-	//	}
-	//
-	//	ret := scope.Stack.Peek()
-	//	if ret.Uint64() != 0 {
-	//		addr := libcommon.BytesToAddress(scope.Stack.Peek().Bytes())
-	//		call.To = addr
-	//		call.Output = t.env.IntraBlockState().GetCode(addr)
-	//	} else if err == nil {
-	//
-	//		call.ErrorString = "internal failure" // TODO(karalabe): surface these faults somehow
-	//	}
-	//} else {
-	//	call.GasUsed = call.GasIn - call.GasCost + call.Gas - gas
-	//	ret := scope.Stack.Peek()
-	//	if ret.Uint64() != 0 {
-	//		call.Output = t.returnData
-	//	} else if err == nil {
-	//		call.ErrorString = "internal failure" // TODO(karalabe): surface these faults somehow
-	//	}
-	//
-	//	// Inject the call into the previous one
-	//	t.traces[topIdx].Calls = append(t.traces[topIdx].Calls, call)
-	//	t.returnData = nil
-	//}
 }
 
 func (t *sentioTracer) CaptureFault(pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, depth int, err error) {
@@ -323,46 +232,12 @@ func (t *sentioTracer) Stop(err error) {
 
 }
 
-//func (t *sentioTracer) Fault(err error) {
-//	topIdx := len(t.traces) - 1
-//	// If the topmost call already reverted, don't handle the additional fault again
-//	if t.traces[topIdx].ErrorString != "" {
-//		return
-//	}
-//
-//	call := t.traces[topIdx]
-//	t.traces = t.traces[0:topIdx]
-//	topIdx--
-//
-//	// Consume all available gas and clean any leftovers
-//	if call.Gas != 0 {
-//		//call.gas = '0x' + bigInt(call.gas).toString(16)
-//		call.GasUsed = call.Gas
-//	}
-//
-//	if topIdx >= 0 {
-//		t.traces[topIdx].Calls = append(t.traces[topIdx].Calls, call)
-//		return
-//	}
-//	//// Last call failed too, leave it in the stack
-//	t.traces = append(t.traces, call)
-//}
-
 func newSentioTracer(name string, ctx *tracers.Context, cfg json.RawMessage) (tracers.Tracer, error) {
 	if name != "sentioTracer" {
 		return nil, errors.New("no tracer found")
 	}
 
-	var config sentioTracerConfig
-	if cfg != nil {
-		if err := json.Unmarshal(cfg, &config); err != nil {
-			return nil, err
-		}
-		log.Info("sentioTracer config", "config", config)
-	}
-
 	return &sentioTracer{
-		config:      config,
 		callsNumber: 1,
 	}, nil
 }
